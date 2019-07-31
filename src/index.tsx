1192050fd86e78e94402bdf5828da7357a00c151
ashahtango <ashah@tangocode.com>
2019-07-31 08:45:17 -0500

    translation done

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import LikeButton from './like_button';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<LikeButton />, document.getElementById('root'));
-
-ReactDOM.render(React.createElement(LikeButton), document.querySelector('#like_button_container'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
